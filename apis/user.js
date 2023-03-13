578fe5871d35eca4e9544bfc75392fb19f7263a7
김홍록 | HongRok <dev@hongrok.net>
2023-03-14 08:29:08 +0900

    대시보드에 카운트들 GET 하는 로직 완성

--- apis/user.js
+++ apis/user.js
@@ -1,5 +1,11 @@
 import { apiAuthInstance } from "@/apis/index";
 
+export async function getCountOfAllUsers() {
+  const { data } = await apiAuthInstance.get("/user/count");
+
+  return data;
+}
+
 export async function getStudentsByGradeAndClass(_grade, _class) {
   const { data } = await apiAuthInstance.get("/user/gradeAndClass", {
     params: { grade: _grade, class: _class },
--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,9 +1,31 @@
 import Layout from "@/components/Layout/Layout";
+import Header from "@/components/common/Header";
+import styled from "styled-components";
+import { useQuery } from "react-query";
+import { getCountOfAllUsers } from "@/apis/user";
 
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 export default function Dashboard() {
+  const { data: count } = useQuery("allCount", getCountOfAllUsers);
+
   return (
     <Layout>
-      <div></div>
+      <Wrap>
+        {count ? (
+          <Header
+            data={[
+              { title: "전체 가입자 수", content: count.all + "명" },
+              { title: "1학년 가입자 수", content: count.grade1 + "명" },
+              { title: "2학년 가입자 수", content: count.grade2 + "명" },
+              { title: "3학년 가입자 수", content: count.grade3 + "명" },
+              { title: "선생님 가입자 수", content: count.teacher + "명" },
+            ]}
+          />
+        ) : null}
+      </Wrap>
     </Layout>
   );
 }
